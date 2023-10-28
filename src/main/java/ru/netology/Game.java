aa613ecf19a0b26b089ccace5d0125d4a2899cc0
Bato <bato19941994@bk.ru>
2023-10-28 13:52:05 +0800

    updateGame

--- src/main/java/ru/netology/Game.java
+++ src/main/java/ru/netology/Game.java
@@ -5,40 +5,23 @@ import java.util.List;
 
 public class Game {
 
-    private Player[] players = new Player[0];
-    List<String> regList = new ArrayList<>();
 
+    List<Player> players = new ArrayList<>();
 
-    public void addPlayer(Player player) {
+    public void register(Player player) {
+        players.add(player);
 
-        Player[] temp = new Player[players.length + 1];
-        for (int i = 0; i < players.length; i++) {
-            temp[i] = players[i];
-        }
-        temp[temp.length - 1] = player;
-        players = temp;
     }
 
     public int round(String playerName1, String playerName2) {
         if (findByName(playerName1) == null) {
             throw new NotRegisteredException(
-                    "Player with name: " + playerName1 + " not not found");
+                    "Player with name: " + playerName1 + " not registration");
         }
-
-
         if (findByName(playerName2) == null) {
             throw new NotRegisteredException(
-                    "Player with name: " + playerName1 + " not found");
+                    "Player with name: " + playerName1 + " not registration");
         }
-
-
-        if ((regList.contains(playerName1) != true)) throw new NotRegisteredException(
-                "Player with name: " + playerName1 + " not registration");
-        if (regList.contains(playerName2) != true) {
-            throw new NotRegisteredException(
-                    "Player with name: " + playerName2 + " not registration");
-        }
-
 
         if (findByName(playerName1).getStrength() > findByName(playerName2).getStrength()) {
             return 1;
@@ -58,7 +41,7 @@ public class Game {
         return null;
     }
 
-    public Player[] findAll() {
+    public List<Player> findAll() {
         return players;
     }
 
--- src/test/java/ru/netology/GameTest.java
+++ src/test/java/ru/netology/GameTest.java
@@ -9,13 +9,8 @@ import java.util.List;
 
 
 public class GameTest {
-
     Game gam = new Game();
-
-
     //  List<String> regList = new ArrayList<>();
-
-
     Player item1 = new Player(3, "Kolya", 7);
     Player item2 = new Player(4, "Petya", 8);
     Player item3 = new Player(6, "Vlad", 9);
@@ -23,27 +18,25 @@ public class GameTest {
     Player item5 = new Player(11, "Slava", 8);
     Player item6 = new Player(33, "Sasha", 10);
     Player item7 = new Player(90, "Evgenii", 20);
+    Player item8 = new Player(96, "Sveta", 2000);
 
     @BeforeEach
     public void setup() {
-        gam.addPlayer(item1);
-        gam.addPlayer(item2);
-        gam.addPlayer(item3);
-        gam.addPlayer(item4);
-        gam.addPlayer(item5);
 
-        gam.regList.add("Kolya");
-        gam.regList.add("Petya");
-        gam.regList.add("Vlad");
-        gam.regList.add("Arkadii");
-        gam.regList.add("Slava");
+        gam.register(item1);
+        gam.register(item2);
+        gam.register(item3);
+        gam.register(item4);
+        gam.register(item5);
+        gam.register(item6);
+        gam.register(item7);
     }
 
 
     @Test
-    public void shouldAllPlayers() {
-        Player[] expected = {item1, item2, item3, item4, item5};
-        Player[] actual = gam.findAll();
+    public void shouldAllRegisteredPlayers() {
+        Player[] expected = {item1, item2, item3, item4, item5, item6, item7};
+        Player[] actual = gam.findAll().toArray(new Player[0]);
 
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -51,7 +44,7 @@ public class GameTest {
     @Test
     public void shouldRoundPlayers2() {
         int expected = 2;
-        int actual = gam.round("Kolya", "Petya");
+        int actual = gam.round(item1.getName(), item2.getName());
 
         Assertions.assertEquals(expected, actual);
     }
@@ -59,7 +52,7 @@ public class GameTest {
     @Test
     public void shouldRoundPlayers1() {
         int expected = 1;
-        int actual = gam.round("Vlad", "Arkadii");
+        int actual = gam.round(item3.getName(), item4.getName());
 
         Assertions.assertEquals(expected, actual);
     }
@@ -67,38 +60,22 @@ public class GameTest {
     @Test
     public void shouldRoundPlayers0() {
         int expected = 0;
-        int actual = gam.round("Slava", "Petya");
+        int actual = gam.round(item5.getName(), item2.getName());
 
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void noShouldRoundPlayers2() {
-        gam.addPlayer(item7);
-        Assertions.assertThrows(NotRegisteredException.class, () -> {
-            gam.round("Kolya", "Evgenii");
-        });
-    }
+    public void ShouldRoundNotRegisterPlayers1() {
 
-    @Test
-    public void noShouldRoundPlayers1() {
-        gam.addPlayer(item6);
-        Assertions.assertThrows(NotRegisteredException.class, () -> {
-            gam.round("Sasha", "Kolya");
-        });
-    }
-
-    @Test
-    public void ShouldRoundNotFoundPlayers1() {
-        gam.addPlayer(item6);
         Assertions.assertThrows(NotRegisteredException.class, () -> {
             gam.round("Sveta", "Kolya");
         });
     }
 
     @Test
-    public void ShouldRoundNotFoundPlayers2() {
-        gam.addPlayer(item6);
+    public void ShouldRoundNotRegisterPlayers2() {
+
         Assertions.assertThrows(NotRegisteredException.class, () -> {
             gam.round("Kolya", "Sveta");
         });
